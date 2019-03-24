cfa0acdab02f0d2d5016cf0df6c7138bc1a68f73
Nigel Yong <kenkenysy@gmail.com>
2019-03-24 19:31:16 -0400

    AmpersandPipe kinda done

--- Assignment3/AmpersandPipe.js
+++ Assignment3/AmpersandPipe.js
@@ -1,7 +1,20 @@
-function getElementAmpersandPipe(){
+var dom = document;
 
+function getElementAmpersandPipe(){
+  var text = dom.getElementById("field");
+
+  addEventListener("blur", convertAmpersandPipe(),false);
 }
 
 function convertAmpersandPipe(){
-  
+  var text = dom.getElementById("field").value;
+  text = text.replace(/\&/g,"and");
+  text = text.replace(/\|/g,"or");
+  dom.getElementById("output").value = text;
 }
+
+window.addEventListener("load", getElementAmpersandPipe(), false );
+
+// function convertAmpersandPipe(){
+ 
+// }
