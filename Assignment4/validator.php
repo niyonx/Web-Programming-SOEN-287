23e349fcd37194a87240695284c13eeee1d1ce2e
Nigel Yong <kenkenysy@gmail.com>
2019-04-14 18:43:01 -0400

    Validator check

--- Assignment4/validator.php
+++ Assignment4/validator.php
@@ -1,35 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Login Welcome</title>
+</head>
+<body>
+
+
 <?php
 /**
  * Created by PhpStorm.
  * User: niyon
- * Date: 12/04/19
- * Time: 4:46 PM
+ * Date: 13/04/19
+ * Time: 10:10 PM
  */
 
-$name = $_GET["name"];
-$tel = $_GET["tel"];
 
-// tel ^\(\d\d\d\)-\d\d\d-\d\d\d$
+$name = $_POST["name"];
+$tel = $_POST["tel"];
+
+// tel ^\(\d\d\d\)-\d\d\d-\d\d\d\d$
 // name ^[A-Z][a-z]+$
-?>
 
-<!doctype html>
-<html lang="`">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h2>
-    Welcome to the contact information validator!
-</h2>
-<form action="validator.php">
-    Last Name: <input type="text" name="name" id="name" size="30"><br><br>
-    Telephone Number: <input type="text" name="tel" id="tel" placeholder="(ddd)-ddd-ddd"><br><br>
-    <input type="submit" value="Validate">
-</form>
+//if (!isset($tel) or empty($tel)  $tel != null && preg_match('^\(\d\d\d\)-\d\d\d-\d\d\d$',$tel)){
+//    echo "Valid telephone number. ";
+//}else{
+//    echo "Invalid telephone number. ";
+//}
+//
+//if (isset($name) && $name != null && preg_match('^[A-Z][a-z]+$',$name)){
+//    echo "Valid last name.";
+//}else{
+//    echo "Invalid last name.";
+//}
+
+
+if (!isset($_POST["name"]) or empty($_POST["name"]) or
+!isset($_POST["tel"]) or empty($_POST["tel"])) {
+    echo "<p>Missing Last name and/or telephone number</p>";
+} else{
+    if (preg_match("/^[A-Z][a-z]+$/", $name)) {
+        echo "<p>Valid last name.</p>";
+    } else {
+        echo "<p>Invalid last name.</p>";
+    }
+    if (preg_match("/^\(\d\d\d\)-\d\d\d-\d\d\d\d$/", $tel)){
+        echo "<p>Valid telephone number</p>";
+    }else{
+        echo "<p>Invalid telephone number</p>";
+    }
+}
+
+
+//$line = "aassDhsh";
+// perform a case-Insensitive search for the word "Vi"
+//
+//if (preg_match("/\b^[A-Z][a-z]+$\b/i", $line)) :
+//    print "Match found!";
+//endif;
+?>
 </body>
 </html>
